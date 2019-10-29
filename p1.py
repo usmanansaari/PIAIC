57b2a406ff73aa69220e26587deae4668f14bb75
usmanansaari <57070343+usmanansaari@users.noreply.github.com>
2019-10-30 02:22:56 +0500

    git init

---
+++ p1.py
@@ -0,0 +1,42 @@
+>>> print ("Hello" , "Word")
+Hello Word
+>>> print name = usman
+  File "<stdin>", line 1
+>>> print name = ("usman")
+  File "<stdin>", line 1
+    print name = ("usman")
+>>> name = "usman ali"
+>>> name
+'usman ali'
+>>> print(name)
+usman ali
+>>> weight = 250
+>>> weight + 50
+300
+>>> weight - 50
+200
+>>> weight / 50
+5.0
+>>> weight * 50
+12500
+>>> popular_number = 4
+>>> print (2+2)
+4
+>>> print (4-3)
+1
+>>> loss = 12-24
+>>> dozens 3 * 12
+  File "<stdin>", line 1
+    dozens 3 * 12
+>>> dozens = 3 * 12
+>>> popular_number = 12 / 4
+>>> num = 0.75
+>>> total = num + 200
+>>> print (num)
+0.75
+>>> print (num + 200)
+200.75
+>>> print (dozens)
+36
+>>> print (popular_number)
+3.0
